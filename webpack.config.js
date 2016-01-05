de0a382945bdfcfadf260e64672ab4c31e33f27f
ouerlianchuang <815094940@qq.com>
2016-01-05 12:58:24 +0800

    Webpack test

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,31 @@
 var vue = require('vue-loader');
+var coffee = require('coffee-loader');
 var webpack = require('webpack');
 
 module.exports = {
+    entry: {
+        app:'./src/main.coffee',
+        vendors: ['vue','jquery','vuex','vue-router']
+    },
+    output: {
+        path: './static',
+        publicPath: '/static/',
+        filename: 'app.js'
+    },
+    module: {
+        loaders: [
+          {
+            test: /\.vue$/,
+            loader: 'vue'
+          },
+          {
+            test: /\.coffee$/,
+            exclude: /node_modules|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
+            loader: 'coffee'
+          }
+        ]
+    },
+   plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+  ]
 };
